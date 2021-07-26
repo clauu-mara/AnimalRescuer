7283a5d5870eacdf3190b39ee3f672b887bd7378
Maraciuc Claudia <claudia.maraciuc@evozon.com>
2021-07-26 14:02:27 +0300

    Objects setup

---
+++ AnimalRescuer1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnimalRescuer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
